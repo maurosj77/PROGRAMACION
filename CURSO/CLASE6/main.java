60a4e7ac68ccc01948ed48b1d1d61e02de371e64
Mauro Luna <mauroluna77@hotmail.com>
2022-03-11 18:40:31 -0300

    .

---
+++ CURSO/CLASE6/main.java
@@ -0,0 +1,7 @@
+int[] numeros = new int[3];
+
+numeros[0] = 1;
+
+for (int i = 0 < numeros.legth; i++){
+System.out.println("el indice es");
+}
--- Portfolio/Angular/Portfolio/src/app/app.module.ts
+++ Portfolio/Angular/Portfolio/src/app/app.module.ts
@@ -4,9 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Prueba1Component } from './prueba1/prueba1.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    Prueba1Component,
+     EncabezadoComponent,
+     AcercaDeComponent,
+     ExperienciaComponent,
+     EducacionComponent,
+     SkillsComponent,
+     ProyectosComponent,
+     FooterComponent
   ],
   imports: [
     BrowserModule,
